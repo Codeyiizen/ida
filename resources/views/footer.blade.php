f7633835d66b5c03f1b31ad18c7a569092b9adbe
subhash018 <59439792+subhash018@users.noreply.github.com>
2024-05-21 15:05:41 +0530

    Adding all page
    Adding all page on git

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,27 @@
+<footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <p>&copy; 2024 Food Based Management System of
+                        Iron Deficiency Anaemia</p>
+                </div>
+                <div class="col-lg-4 text-lg-right">
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="disclaimer.html">Disclaimer</a>
+                        </li>
+                        <li><a href="developed.html">Developed By</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Javascript -->
+    <script src="assets/javascripts/jquery-1.11.2.min.js"></script>
+    <script src="assets/javascripts/popper.js"></script>
+    <script src="assets/javascripts/bootstrap.bundle.js"></script>
+    <script src="assets/javascripts/bootstrap.js"></script>
+    <script src="assets/javascripts/all.js"></script>
+    <script src="assets/javascripts/meanmenu.js"></script>
+    <!-- Custom JS Code for this page -->
+    <script src="assets/javascripts/main.js"></script>
